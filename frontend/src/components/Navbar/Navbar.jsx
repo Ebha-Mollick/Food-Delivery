86d911b61d0477c61be19ca32b76f4643064b218
Ebha-Mollick <ebhamollick@gmail.com>
2024-11-13 15:05:06 +0530

    added home page

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt=""  className='logo'/>
+        <ul className='navbar-menu'>
+        <Link to='/'><li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>home</li></Link>
+        <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+        </ul>
+        <div className='navbar-right'>
+            <img src={assets.search_icon} alt="" />
+            <div className='navbar-search-icon'>
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <div className="dot">
+
+                </div>
+            </div>
+            <button>sign in</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
